6dd15d447047a8155e814010e013a876fe131aa8
ryanmonro <ryan@ryanmonro.info>
2019-08-11 21:46:20 +1000

    Bounces working, added tonal.js for the future

--- sketch.js
+++ sketch.js
@@ -1,41 +1,21 @@
-// This is cool, but...
-/* heading back to the original idea at the museum:
-it could draw the squares gradually, drawing one per draw() call
-and play a note on each one
-mouse position still determines proportion of colours
-you will hear the difference more than see it
-10 calls per second, 5 seconds per line
-if it's too tedious let's draw multiple lines simultaneously
-when the grid is full, start again from the top without clearing
-Next question: what happens on mobile?
-*/
-
 'use strict';
 
 var model, reverb;
+// console.log(Tonal.transpose("C4", "2M"))
 
 function setup() {
   createCanvas(window.innerWidth / 2, window.innerHeight / 2);
   frameRate(30);
   colorMode(HSB, 360, 100, 100);
   ellipseMode(RADIUS);
-  // sloop = new p5.SoundLoop(soundLoop, 0.125);
   reverb = new p5.Reverb();
   reverb.set(1.7);
   reverb.connect(p5.soundOut);
   model = new Model();
 }
 
-function soundLoop(time){
-  // synth.play(note, 0.1, 0, 0.1);
-}
-
 function draw() {
   background(60);
-  // fill(random(360), 100, 100);
-  // noStroke();
-  // ellipse(250, 250, 30, 30);
-
   model.move();
 }
 
@@ -51,7 +31,7 @@ function keyPressed(){
 }
 
 function randomNote() {
-  return pickOne(['C', 'D', 'E','F', 'G', 'A','B']) + pickOne(["4","5","6"]);
+  return random(['C', 'D','F', 'G', 'A']) + random(["4","5","6"]);
 }
 
 var Model = function () {
@@ -59,27 +39,28 @@ var Model = function () {
   this.minPopulation = 3;
   this.maxPopulation = 8;
   this.lastBallAddedTime = 0;
-  this.lastBallRemovedTime = 0;
-  this.ballAddInterval = 3000;
+  this.ballAddInterval = 1000;
 }
 
 Model.prototype.add = function(x, y) {
-  if (this.balls.length >= this.maxPopulation) {
-    return
+  if(this.balls.length <= this.maxPopulation){
+    this.balls.push(new Ball(x || width / 2, y || height / 2, this.balls));
+    this.lastBallAddedTime = Date.now();
   }
-  this.balls.push(new Ball(x || width / 2, y || height / 2, this.balls));
-  this.lastBallAddedTime = Date.now();
+}
+
+Model.prototype.populate = function() {
+  if (
+      // Date.now() > this.lastBallRemovedTime + this.ballAddInterval &&
+      Date.now() > this.lastBallAddedTime + this.ballAddInterval &&
+      this.balls.length < this.minPopulation
+    ) {
+    this.add()
+  }
 }
 
 Model.prototype.move = function() {
-  if (
-      Date.now() > this.lastBallRemovedTime + this.ballAddInterval &&
-      Date.now() > this.lastBallAddedTime + this.ballAddInterval
-       &&
-      this.balls.length < this.minPopulation
-    ) {
-    this.add();
-  }
+  this.populate();
   var dead = [];
   for (var b = 0; b < this.balls.length; b++){
     var ball = this.balls[b];
@@ -92,7 +73,9 @@ Model.prototype.move = function() {
   // bring out your dead
   for(var i = 0; i < dead.length; i++){
     this.balls.splice(dead[i], 1);
-    this.lastBallRemovedTime = Date.now();
+    if (this.balls.length < this.minPopulation) {
+      this.lastBallAddedTime = Date.now();
+    }
   }
 }
 
@@ -104,6 +87,7 @@ var Ball = function (x, y, balls) {
   this.velocity = createVector(xVel, 10 - xVel)
   this.nextVelocity = createVector(this.velocity.x, this.velocity.y);
   this.lifeSpan = 9;
+  this.mass = 1;
   this.hue = random(360);
   this.note = randomNote();
   this.synth = new p5.MonoSynth();
@@ -136,47 +120,58 @@ Ball.prototype.collision = function() {
       var distance = this.position.dist(ball.position)
       var minDistance = this.radius + ball.radius
       if (distance < minDistance ) {
-        // balls colliding. First, move them apart.
-        // var overlap = minDistance - distance
-        var difference = p5.Vector.sub(this.position, ball.position)
-        var angle = p5.Vector.sub(this.position, ball.position).heading()
-        var x = minDistance * cos(angle)
-        var y = minDistance * sin(angle) 
-        var newDifference = createVector(x, y)
-        var change = p5.Vector.sub(difference, newDifference)
-        // ball.position.add(change)
-        ball.position.add(difference.x - x, difference.y - y)
-
-        // change their velocities
-        if ((this.velocity.x > 0 && ball.velocity.x < 0) ||
-            (this.velocity.x < 0 && ball.velocity.x > 0)) {
-          this.velocity.x = this.velocity.x * -1;
-        }
-        if ((this.velocity.y > 0 && ball.velocity.y < 0) ||
-            (this.velocity.y < 0 && ball.velocity.y > 0)) {
-          this.velocity.y = this.velocity.y * -1;
-        }
-        // needsBounce = true;
+        this.changeVectors(ball);
+        needsBounce = true;
       }
     }
   }
   needsBounce && this.bounce();
 }
 
+Ball.prototype.changeVectors = function(ball) {
+  // balls colliding. First, move them apart.
+  var minDistance = this.radius + ball.radius
+  var angle = p5.Vector.sub(this.position, ball.position).heading()
+  var difference = p5.Vector.sub(this.position, ball.position)
+  var newPos = createVector(minDistance * cos(angle), minDistance * sin(angle))
+  ball.position.add(difference.x - newPos.x, difference.y - newPos.y)
+  
+  // Thank you Adam Brookes http://cobweb.cs.uga.edu/~maria/classes/4070-Spring-2017/Adam%20Brookes%20Elastic%20collision%20Code.pdf
+  // Normalise distance vector, find tangent vector
+  var normalVector = difference.normalize()
+  var tangentVector = createVector(normalVector.y * -1, normalVector.x)
+  
+  // resolve velocity vectors into normal and tangential
+  // ball scalar normal direction
+  var thisScalarNormal = normalVector.dot(this.velocity)
+  var ballScalarNormal = normalVector.dot(ball.velocity)
+  // scalar velocities in tangential direction
+  var thisScalarTangential = tangentVector.dot(this.velocity)
+  var ballScalarTangential = tangentVector.dot(ball.velocity)
+
+  // Find new velocities
+  var thisScalarNormalAfter = (thisScalarNormal * (this.mass - ball.mass) + 2 * ball.mass * ballScalarNormal) / (this.mass + ball.mass);
+  var ballScalarNormalAfter = (ballScalarNormal * (ball.mass - this.mass) + 2 * this.mass * thisScalarNormal) / (this.mass + ball.mass);
+
+  // convert to vectors
+  var thisScalarNormalAfterVector = p5.Vector.mult(normalVector, thisScalarNormalAfter);
+  var ballScalarNormalAfterVector = p5.Vector.mult(normalVector, ballScalarNormalAfter);
+  var thisScalarNormalVector = p5.Vector.mult(tangentVector, thisScalarTangential);
+  var ballScalarNormalVector = p5.Vector.mult(tangentVector, ballScalarTangential);
+
+  // add normal and tangentials for each ball
+  this.velocity = p5.Vector.add(thisScalarNormalVector, thisScalarNormalAfterVector);
+  ball.velocity = p5.Vector.add(ballScalarNormalVector, ballScalarNormalAfterVector);
+}
+
 Ball.prototype.draw = function() {
   fill(this.hue, 100, 100);
   noStroke();
   ellipse(this.position.x, this.position.y, this.radius, this.radius);
-  fill(0,0,0);
-  textSize(32);
-  text(this.lifeSpan, this.position.x, this.position.y);
 }
 
 Ball.prototype.bounce = function() {
+  // this.synth.pan(1)
   this.synth.play(this.note, 0.1, 0.01, 0.1);
   this.lifeSpan -= 1;
-}
-
-function pickOne(arr){
-  return shuffle(arr)[0];
 }
